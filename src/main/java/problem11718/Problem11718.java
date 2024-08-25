67426c1467e7482fad4e14351c153773f40565b1
Denis Bunak <xisi926@ukr.net>
2024-08-25 12:04:03 +0300

    Before refactoring method SecondVariant of Problem11718 class

--- src/main/java/problem11718/Problem11718.java
+++ src/main/java/problem11718/Problem11718.java
@@ -1,67 +1,117 @@
 package problem11718;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Problem11718 {
-
+//TODO The problem is not done
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
 
         scanner.nextInt();
-        int numberOfeRequest = scanner.nextInt();
+        int numberOfRequest = scanner.nextInt();
         scanner.nextLine();
         String word = scanner.nextLine();
         char[] letters = word.toCharArray();
         List<Integer> firstIndexes = new ArrayList<>();
         List<Integer> lastIndexes = new ArrayList<>();
+        List<Long> results = new ArrayList<>();
 
-        for (int i = 0; i < numberOfeRequest; i++) {
+        for (int i = 0; i < numberOfRequest; i++) {
             firstIndexes.add(scanner.nextInt());
             lastIndexes.add(scanner.nextInt());
         }
 
-        List<Integer> results = new ArrayList<>();
-        for (int i = 0; i < numberOfeRequest; i++) {
-            int countOfConcurrence = 0;
-            List<Integer> countOfConcurrrences = new ArrayList<>();
+/*
+        for (int i = 0; i < numberOfRequest; i++) {
+            long startTime = System.currentTimeMillis();
+            List<Long> resultsOfFirstVariant = new ArrayList<>();
             List<Character> checkedLetters = new ArrayList<>();
+            long resultOfFirstVariant = 0;
             int startIndex = firstIndexes.get(i) - 1;
-            int lastIndex = lastIndexes.get(i) - 1;
+            int lastIndex = lastIndexes.get(i);
 
-            for (int j = startIndex; j <= lastIndex; j++) {
+            for (int j = startIndex; j < lastIndex; j++) {
                 char letter = letters[j];
-                int concurrence = 1;
 
                 if (!checkedLetters.contains(letter)) {
-                    for (int k = j + 1; k <= lastIndex; k++) {
-
-                        if (letter == letters[k]) {
-                            if (concurrence > 0) {
-                                countOfConcurrence++;
-                            }
-                            concurrence++;
-                        } else {
-                            concurrence = 0;
-                        }
-                    }
+                    resultOfFirstVariant += firstVariant(word, letter, j, lastIndex);
                     checkedLetters.add(letter);
                 }
-                countOfConcurrrences.add(countOfConcurrence);
             }
 
-            Optional<Integer> max = countOfConcurrrences.stream()
-                    .max(Integer::compareTo);
+            //Результаты первого варианта
+            System.out.println("Время первого варианта = " + (System.currentTimeMillis() - startTime));
+            System.out.println("Первый вариант, " + "Запрос = " + (i + 1) + ", результат = " + resultOfFirstVariant);
+            resultsOfFirstVariant.add(resultOfFirstVariant);
+        }*/
 
-            if (max.isPresent()) {
-                results.add(max.get());
-            } else {
-                results.add(countOfConcurrence);
+        //Second Variant
+        for (int i = 0; i < numberOfRequest; i++) {
+            List<Character> checkedLetters = new ArrayList<>();
+            long resultOfSecondVariant = 0;
+            int startIndex = firstIndexes.get(i) - 1;
+            int lastIndex = lastIndexes.get(i);
+
+            for (int j = startIndex; j < lastIndex; j++) {
+                char letter = letters[j];
+
+                if (!checkedLetters.contains(letter)) {
+                    resultOfSecondVariant += secondVariant(word, letter, startIndex, lastIndex);
+                    checkedLetters.add(letter);
+                }
             }
+
+/*            //Результаты второго варианта
+            System.out.println("Время второго варианта = " + (System.currentTimeMillis() - startTime));
+            System.out.println("Второй вариант, " + "Запрос = " + (i + 1) + ", результат = " + resultOfSecondVariant);*/
+            results.add(resultOfSecondVariant);
         }
         results.forEach(System.out::println);
+    }
+
+    public static long firstVariant(String word, char letter, int startIndex, int lastIndex) {
+        char[] letters = word.toCharArray();
+        int concurrence = 1;
+        int result = 0;
+
+        for (int k = startIndex + 1; k < lastIndex; k++) {
+
+            if (letter == letters[k]) {
+                if (concurrence > 0) {
+                    result++;
+                }
+                concurrence++;
+            } else {
+                concurrence = 0;
+            }
+        }
+        return result;
+    }
+
+    public static long secondVariant(String word, int startIndex, int lastIndex) {
+        long result = 0;
+        String wordForChecking = word.substring(startIndex, lastIndex);
+        String[] letters = wordForChecking.split(String.valueOf(letter));
+        String str = String.valueOf(letter) + String.valueOf(letter);
+
+        for (String letter : letters) {
+
+        }
+
+        if (letters.length == 0 & (lastIndex - startIndex) > 1) {
+            result = (wordForChecking.length() - 1);
+        } else {
+
+            Pattern pattern = Pattern.compile(str);
+            Matcher m = pattern.matcher(wordForChecking);
+
+            while (m.find()) {
+                result++;
+            }
+        }
+        return result;
     }
 }
--- src/main/java/problem11718/Test.java
+++ src/main/java/problem11718/Test.java
@@ -1,20 +1,18 @@
 package problem11718;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Test {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        String[] s = "mississippi".split("s");
+        System.out.println(Arrays.toString(s));
+        long count = s.length - 1;
 
-        String s = scanner.nextLine();
-        String[] s1 = s.split(" ");
-        int numberOfLetters = Integer.valueOf(s1[0]);
-        System.out.println("numberOfLetters = " + numberOfLetters);
-        int numberOfRequest = Integer.valueOf(s1[1]);
-        System.out.println("numberOfRequest = " + numberOfRequest);
-        System.out.println("scanner.nextLine() = " + scanner.nextLine());
+        for (String s1 : s) {
+            System.out.println(s1);
+        }
+        System.out.println("count = " + count);
 
     }
-
 }
